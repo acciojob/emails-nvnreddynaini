9055dceb4f9f7f7c76949407cff0103a5f7dbe2b
nvnreddynaini <nvnreddynaini@gmail.com>
2022-12-24 23:40:56 +0530

    Fith Commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -24,29 +24,25 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        int count = 0;
-        int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0;
-        for(int i = 0; i < newPassword.length(); i++) {
-            if(oldPassword == getPassword()){
-                if(newPassword.length() >= 8 && flag1 == 0){
-                    count++;
-                    flag1 = 1;
-                }
-                else if ((newPassword.charAt(i) >= 'A' && newPassword.charAt(i) <= 'Z') && (flag2 == 0)) {
-                    count++;
+        int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0,flag5=0;
+        if(oldPassword == getPassword()){
+            if(newPassword.length() >= 8){
+                flag1 = 1;
+            }
+            for(int i = 0; i < newPassword.length(); i++) {
+                if ((newPassword.charAt(i) >= 'A' && newPassword.charAt(i) <= 'Z')) {
                     flag2 = 1;
-                } else if ((newPassword.charAt(i) >= 'a' && newPassword.charAt(i) <= 'z') && (flag3 == 0)){
-                    count++;
+                } else if ((newPassword.charAt(i) >= 'a' && newPassword.charAt(i) <= 'z')){
                     flag3 = 1;
-                } else if ((newPassword.charAt(i) >= 48 && newPassword.charAt(i) <= 57) && (flag4 == 0)) {
-                    count++;
+                } else if ((newPassword.charAt(i) >= 48 && newPassword.charAt(i) <= 57)) {
                     flag4 = 1;
                 }else{
-                    count++;
+                    flag5 = 1;
                 }
             }
         }
-        if(count >= 5 && flag1 == 1 && flag2 ==1 && flag3 == 1 && flag4 ==1){
+
+        if((flag1+flag2+flag3+flag4+flag5)==5){
             password = newPassword;
         }
     }
