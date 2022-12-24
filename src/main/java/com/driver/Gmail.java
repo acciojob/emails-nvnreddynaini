98bd41f48de94918a0d5fe3fe39c9507d3ab1f7c
nvnreddynaini <nvnreddynaini@gmail.com>
2022-12-24 23:57:07 +0530

    seven Commit

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -132,6 +132,6 @@ public class Gmail extends Email {
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
-        return inboxCapacity;
+        return inboxCapacity-getInboxSize();
     }
 }
